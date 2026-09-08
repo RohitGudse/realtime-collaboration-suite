fbf1b8a3e86cf8ac6bc23aac0eaa8ae87ba3d64d
rohit gudse <rohitgudse@gmail.com>
2026-09-08 17:07:35 +0530

    Create reusable NoteItem component

--- client/notes-client/src/COMPONENTS/NoteItem.jsx
+++ client/notes-client/src/COMPONENTS/NoteItem.jsx
@@ -1,4 +1,32 @@
-const NoteItem = ({ title }) => {
-  return <div>{title}</div>;
+import React from "react";
+
+const NoteItem = ({ title, description, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick(title);
+    }
+  };
+
+  return (
+    <article
+      className="note-item"
+      onClick={handleClick}
+      role={onClick ? "button" : undefined}
+      tabIndex={onClick ? 0 : undefined}
+    >
+      <div className="note-item__content">
+        <h3 className="note-item__title">
+          {title || "Untitled Note"}
+        </h3>
+
+        {description && (
+          <p className="note-item__description">
+            {description}
+          </p>
+        )}
+      </div>
+    </article>
+  );
 };
+
 export default NoteItem;
